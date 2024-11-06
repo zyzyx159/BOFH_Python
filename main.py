db3de9b90f026ae028241497791c6e5ae4fdd442
Daniel Stellmon <Dstellm@gmail.com>
2024-11-06 03:56:24 -0600

    SQLite3

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from playwright.sync_api import sync_playwright, playwright
+import sqlite3
+
+sqliteConnection = sqlite3.connect('bofh')
+cursor = sqliteConnection.cursor()
 
 def run(playwright: playwright):
     start_url = "https://www.theregister.com/offbeat/bofh/"
@@ -9,7 +13,8 @@ def run(playwright: playwright):
 
     for link in page.locator("a[class=story_link]").all():
         url = link.get_attribute("href")
-        print(url)
+        if "bofh" in url:
+            cursor.execute('select count ')
 
 with sync_playwright() as playwright:
     run(playwright)
