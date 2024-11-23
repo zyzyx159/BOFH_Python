eb2b79e33e03f0fa5f4fbc085524177e5aa0d356
Daniel Stellmon <Dstellm@gmail.com>
2024-11-23 04:46:05 -0600

    wrote file to scrape stories

---
+++ getEpisode.py
@@ -0,0 +1,47 @@
+from playwright.sync_api import sync_playwright, playwright
+import database
+import episode
+
+#get all the links of episodes I have not downloaded yet
+bofhDB = database.database()
+bofhDB.download()
+
+def run(playwright: playwright, URL):
+    chrome = playwright.chromium
+    browser = chrome.launch(headless=False)
+    page = browser.new_page()
+    page.goto(URL)
+
+    #make an episode instance for each
+    epi = episode.episode(URL)
+    topCol = page.query_selector("#top-col-story")
+    titles = topCol.query_selector_all("h1")
+    epi.setTitle(titles[0].inner_text())
+
+    subtitles = topCol.query_selector_all("h2")
+    epi.setSubtitle(subtitles[0].inner_text())
+
+    authors = topCol.query_selector_all(".byline")
+    epi.setAuthor(authors[0].inner_text())
+
+    published = topCol.query_selector_all(".dateline")
+    epi.setPubDate(published[0].inner_text())
+
+    pageBody = page.query_selector("#body")
+    story = pageBody.query_selector_all("p")
+    epiStory = "\n"
+    for p in story:
+        epiStory = epiStory + p.inner_text() + "\n"
+    epi.setStory(epiStory)
+
+    return epi
+
+    #epi.printShort()
+    # epi.printStory()
+
+#make a PlayWright instance for each
+with sync_playwright() as playwright:
+    for link in bofhDB.download():
+        play = run(playwright, link[0])
+        print(play.getDownloaded())
+        bofhDB.update(play)
