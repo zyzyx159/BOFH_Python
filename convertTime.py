85cf4dc87f14cd83811583796bb6feb03d9025c6
Daniel Stellmon <Dstellm@gmail.com>
2024-11-23 05:39:24 -0600

    added notes

--- convertTime.py
+++ convertTime.py
@@ -2,12 +2,14 @@ from datetime import datetime
 import calendar
 import time
 
+#website string
 dateString = "Fri 22 Nov 2024 // 09:25 UTC"
 
+#human format
 dt_object = datetime.strptime(dateString, "%a %d %b %Y // %H:%M %Z")
 
+#date time
 timeStamp = calendar.timegm(dt_object.utctimetuple())
 
-print("Unix time: " + str(timeStamp))
-formattedTime = time.strftime('%Y-%m-%d %H:%M', time.gmtime(timeStamp))
-print("Human time: " + formattedTime)
+#just date
+formattedTime = time.strftime('%Y-%m-%d', time.gmtime(timeStamp))
