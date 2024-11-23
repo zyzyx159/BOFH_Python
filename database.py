2e29072a9cd45e447ecfe2097b8c086ca5d23da3
Daniel Stellmon <Dstellm@gmail.com>
2024-11-23 04:45:26 -0600

    added functions to support inserting episodes

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import episode
 
 class database:
     def __init__(self):
@@ -7,24 +8,43 @@ class database:
         self.cursor = self.sqliteConnection.cursor()
 
         self.createQuery = '''create table if not exists bofh(link text primary key,
-            downloaded text, title text,
+            downloaded text, 
+            title text,
             subtitle text,
             author text,
             pubDate text,
             story text);'''
         self.countQuery = "select count(*) from bofh where link = ?"
-        self.insertQuery = "insert into bofh (link, downloaded) values (?, 'false');"
+        self.insertQuery = "insert into bofh (link, downloaded) values (?, 'False');"
+        self.downloadQuery = "select link from bofh where downloaded = 'False' limit 1;"
+        self.updateQuery = '''update bofh
+            set downloaded = ?,
+            title = ?,
+            author = ?,
+            pubDate = ?,
+            story = ?
+            where link = ?;'''
 
         self.cursor.execute(self.createQuery)
 
     def count(self, URL):
         self.cursor.execute(self.countQuery, (URL,))
         count = self.cursor.fetchall()
-        #print(count[0][0])
         return count[0][0]
 
     def insert(self, URL):
         self.cursor.execute(self.insertQuery, (URL,))
+        self.sqliteConnection.commit()
+
+    def download(self):
+        self.cursor.execute(self.downloadQuery)
+        downloadList = self.cursor.fetchall()
+        return downloadList
+
+    def update(self, episode):
+        self.cursor.execute(self.updateQuery, (episode.getDownloaded(),
+            episode.getTitle(), episode.getAuthor(), episode.getPubDate(), 
+            episode.getStory(), episode.getURL(),))
         self.sqliteConnection.commit()
 
     def close(self):
