2b180edd61215f6c94b141f47171dc544ad0adaa
Daniel Stellmon <Dstellm@gmail.com>
2024-11-25 04:29:56 -0600

    creating an time conversion class.

--- convertTime.py
+++ convertTime.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import calendar
 import time
+import pytz
 
 #website string
 dateString = "Fri 22 Nov 2024 // 09:25 UTC"
@@ -13,3 +14,10 @@ timeStamp = calendar.timegm(dt_object.utctimetuple())
 
 #just date
 formattedTime = time.strftime('%Y-%m-%d', time.gmtime(timeStamp))
+
+centralTZ = pytz.timezone('US/Central')
+eastTZ = pytz.timezone('US/Eastern')
+
+print("human format: " + str(dt_object))
+print("date time: " + str(timeStamp))
+print("just date:" + str(formattedTime))
