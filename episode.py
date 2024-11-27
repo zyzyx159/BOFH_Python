5def1f715c7733114838c3a94506af61cd8d11a8
Daniel Stellmon <Dstellm@gmail.com>
2024-11-27 03:48:29 -0600

    added date time conversions

--- episode.py
+++ episode.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import pytz
+
 class episode:
     def __init__(self, URL):
         self.url = URL
@@ -12,9 +15,11 @@ class episode:
     def setAuthor(self, author):
         self.author = author
 
-    def setPubDate(self, pubDate):
-        self.pubDate = pubDate
-
+    def setPubDate(self, pubDate): #demo pubDate: "Fri 22 Nov 2024 // 09:25 UTC"
+        importFormat = ("%a %d %b %Y // %H:%M %Z")
+        dt = datetime.strptime(pubDate, importFormat)
+        self.pubDate = dt.replace(tzinfo=pytz.utc)
+        
     def setStory(self, story):
         self.story = story
 
@@ -33,8 +38,15 @@ class episode:
     def getAuthor(self):
         return self.author
 
-    def getPubDate(self):
-        return self.pubDate
+    def getPubDate(self, format, TZ):
+        forShort = ("%Y-%m-%d") #2024-11-22
+        forLong = ("%A, %B %d %Y at %H:%M %Z") #Friday, November 22 2024 at 09:25 UTC
+        if format is 'short':
+            return self.pubDate.strftime(forShort)
+        elif format is 'long' and TZ is None:
+            return self.pubDate.strftime(forLong)
+        elif format is 'long' and TZ is not None:
+            return self.pubDate.astimezone(pytz.timezone(TZ)).strftime(forLong)
 
     def getStory(self):
         return self.story
@@ -43,7 +55,9 @@ class episode:
         print("Title = " + self.title)
         print("Subtitle = " + self.subtitle)
         print("Author = " + self.author)
-        print("Published on = "+ self.pubDate)
+        print("Published on (short) = "+ self.pubDate, 'short')
+        print("Published on (long) = " + self.pubDate, 'long')
+        print("Published on (long, central) = " + self.pubDate, 'long', 'US/Central')
 
     def printStory(self):
         print("Story = " + self.story)
