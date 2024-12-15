da69d9d18faa569c44650e49ed70e04066a1d5c3
Daniel Stellmon <Dstellm@gmail.com>
2024-12-15 04:00:00 -0600

    exporter works now

--- export.py
+++ export.py
@@ -1,5 +1,37 @@
 import episode
+import os
 
 class export:
     def __init__(self, epi: episode):
-        epi.printShort()
+        self.epi = epi
+        self.writeFile()
+    
+    def getPath(self):
+        path = os.path.join(os.path.dirname(__file__), "output", self.epi.formatPubDate('year'))
+        if not os.path.exists(path):
+            os.makedirs(path)
+        return path
+
+    def getFileName(self):
+        fileName = self.epi.getEpisodeNum() + "_" + self.epi.getTitle() + ".html"
+        return fileName
+
+    def getEpiString(self):
+        epiString = '''<html>
+            <head>
+            <title> ''' + self.epi.getEpisodeNum() + ''' - ''' + self.epi.getTitle() + '''</title>
+            </head>
+            <body>
+            <h1>''' + self.epi.getTitle() + ''' </h1> 
+            <h2>''' + self.epi.getSubtitle() + ''' </h2>
+            <h3>''' + self.epi.getEpisodeNum() + ''' </h3>
+            <br> horned phone logo here </br>''' + self.epi.getStory() + '''
+            Written by: ''' + self.epi.getAuthor() + '''
+            <br> Posted to: <a href="''' + self.epi.getURL() + '''"> ''' + self.epi.getURL() + '''</a> </br>
+            Posted on: ''' + self.epi.formatPubDate('long') + '''
+            </body> </html>'''
+        return epiString
+    
+    def writeFile(self):
+        with open(os.path.join(self.getPath(), self.getFileName()), "w") as f:
+            f.write(self.getEpiString())
