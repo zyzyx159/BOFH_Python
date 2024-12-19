025c73cbcdda41cb67c9c791ecb95fdbe0f50c18
Daniel Stellmon <Dstellm@gmail.com>
2024-12-19 02:36:27 -0600

    started export

--- export.py
+++ export.py
@@ -1,4 +1,17 @@
 import episode
+import os
+
+#accept an episode as part of init
+
+#create file named year + episode number
+
+class chapter():
+    def __init__(self, episode):
+        self.epi = episode.episode
+
+        #if folder year not exists create it
+        path = self.epi
+
 
 fileName = 'test'
 file = open()
