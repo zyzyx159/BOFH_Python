9285b2b8e49a699742828c085fabfea4922c253b
Daniel Stellmon <Dstellm@gmail.com>
2024-12-19 05:54:33 -0600

    runs export

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import episode
 import export
 import os
+import database
 
-epi = episode.episode("https://www.theregister.com/2024/11/22/bofh_2024_episode_22/")
-epi.DBInit()
+# epi = episode.episode("https://www.theregister.com/2000/05/09/wanted_bastard_operators_to_man/")
+# epi.DBInit()
 
-out = export.export(epi)
-out.writeFile()
+# out = export.export(epi)
+# out.writeFile()
+
+bofhDB = database.database()
+for link in bofhDB.getLinks():
+    epi = episode.episode(link[0])
+    epi.DBInit()
+    out = export.export(epi)
+    out.writeFile()
