4167db0f4b76792599fb01635bfc198467e4ff53
Daniel Stellmon <Dstellm@gmail.com>
2024-12-20 01:51:50 -0600

    just get home page

--- getLinks.py
+++ getLinks.py
@@ -10,30 +10,12 @@ def run(playwright: playwright, startURL):
     page = browser.new_page()
     page.goto(startURL)
 
-    bofhLink = 0
-    newLink = 0
-        
     for link in page.locator("a[class=story_link]").all():
         url = link.get_attribute("href")
         if "bofh" in url: # this line does not work for the very early episodes, but is not needed for the archive
-            bofhLink += 1
             if bofhDB.count(urlBase + url) == 0:
-                newLink += 1
                 bofhDB.insert(urlBase + url,)
-    if bofhLink > newLink:
-        nextPage = False
-    elif bofhLink == 0:
-        nextPage = False
-    else:
-        nextPage = True
-    return nextPage
 
 with sync_playwright() as playwright:
     current = run(playwright, startURL="https://www.theregister.com/offbeat/bofh/")
-    earlier = 1
-    if current == True:
-        archive = True
-        while archive == True:
-            archive = run(playwright, startURL="https://www.theregister.com/offbeat/bofh/earlier/" + str(earlier) + "/")
-            earlier += 1
     bofhDB.close()
