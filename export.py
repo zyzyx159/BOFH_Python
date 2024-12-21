c70df17a3d4339b408d1c4e72e1cd5cce4de138c
Daniel Stellmon <Dstellm@gmail.com>
2024-12-21 05:51:38 -0600

    large refactor

--- export.py
+++ export.py
@@ -1,9 +1,13 @@
 import episode
 import os
+import database
+from ebooklib import epub
+import ebooklib
 
 class export:
-    def __init__(self, epi: episode):
-        self.epi = epi
+    def __init__(self):
+        #self.epi = epi
+        self.DB = database.database()
     
     def getPath(self):
         path = os.path.join(os.path.dirname(__file__), "output", self.epi.formatPubDate('year'))
@@ -11,26 +15,37 @@ class export:
             os.makedirs(path)
         return path
 
-    def getFileName(self):
-        fileName = self.epi.getEpisodeNum() + "_" + self.epi.getTitle() + ".html"
-        return fileName
-
-    def getEpiString(self):
-        epiString = '''<html>
-            <head>
-            <title> ''' + self.epi.getEpisodeNum() + ''' - ''' + self.epi.getTitle() + '''</title>
-            </head>
-            <body>
-            <h1>''' + self.epi.getTitle() + ''' </h1> 
-            <h2>''' + self.epi.getSubtitle() + ''' </h2>
-            <h3>''' + self.epi.getEpisodeNum() + ''' </h3>
-            ''' + self.epi.getStory() + '''
-            Written by: ''' + self.epi.getAuthor() + '''
-            <br> Posted to: <a href="''' + self.epi.getURL() + '''"> ''' + self.epi.getURL() + '''</a> </br>
-            Posted on: ''' + self.epi.formatPubDate('long') + '''
-            </body> </html>'''
-        return epiString
-    
-    def writeFile(self):
+    def writeHTML(self):
         with open(os.path.join(self.getPath(), self.getFileName()), "w") as f:
             f.write(self.getEpiString())
+
+    def buildBook(self):
+        book = epub.EpubBook()
+
+        #base set up
+        book.set_identifier('BOFH Omnibus')
+        book.set_title('The BOFH Omnibus')
+        book.set_language('en')
+        book.add_author('Simon Travaglia')
+
+        chapters = self.DB.getLinks()
+        book.spine.insert(1, 'nav')
+        i=2 #counter to add chapters with
+        toc = [] #list of chapters for Table of Contents
+        for chapter in chapters:
+            epi = episode.episode(chapter[0])
+            epi.DBInit()
+            chap = epub.EpubHtml(title=epi.getTitle(), 
+                file_name=epi.getFileName(),
+                lang='en')
+            chap.content=epi.getEpiString()
+            book.add_item(chap)
+            book.spine.insert(i, chap)
+            toc.append(epub.Link(chap.file_name, chap.title, 'chapter'))
+            i=i+1
+        book.toc = (epub.Link('nav.xhtml', 'Table of Contents', 'toc'),
+            (epub.Section('Chapters'),toc),)
+        book.add_item(epub.EpubNcx())
+        book.add_item(epub.EpubNav())
+
+        epub.write_epub('bofh.epub', book)
