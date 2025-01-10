28470784a642893155aade706a74ae4df582ca44
Daniel Stellmon <Dstellm@gmail.com>
2025-01-10 03:33:35 -0600

    removed comments and junk code

--- dataClean/bjashEpisode.py
+++ dataClean/bjashEpisode.py
@@ -1,24 +1,8 @@
-# from datetime import datetime
-# import pytz
-# import database
-# import string
-
 class episode:
 #region init
     def __init__(self, URL):
         self.url = URL
         self.downloaded = "True"
-
-    # def DBInit(self):
-    #     bofhDB = database.database()
-    #     row = bofhDB.episodeFromDB(self.getURL())
-    #     self.episodeNum = row[0][0]
-    #     self.title = row[0][1]
-    #     self.subtitle = row[0][2]
-    #     self.author = row[0][3]
-    #     self.pubDate = row[0][4]
-    #     self.story = row[0][5]
-    #     bofhDB.close
 #endregion
 
 #region set
@@ -34,11 +18,8 @@ class episode:
     def setAuthor(self, author):
         self.author = author
 
-    def setPubDate(self, pubDate): #demo pubDate: "Fri 22 Nov 2024 // 09:25 UTC"
-        # importFormat = ("%a %d %b %Y // %H:%M %Z")
-        # dt = datetime.strptime(pubDate, importFormat)
+    def setPubDate(self, pubDate): 
         self.pubDate = pubDate
-        #self.pubDate = dt.astimezone(pytz.timezone('UTC'))
     
     def setPubYear(self, pubYear):
         self.pubYear = pubYear
@@ -76,31 +57,6 @@ class episode:
 #endregion
 
 #region methods
-    # def formatPubDate(self, format):
-    #     dbFormat = ("%Y-%m-%d %H:%M:%S")
-    #     strDate = datetime.strptime(self.pubDate, dbFormat)
-    #     forYear = ("%Y") #2024
-    #     forShort = ("%Y-%m-%d") #2024-11-22
-    #     forLong = ("%A, %B %d, %Y at %H:%M %Z") #Friday, November 22 2024 at 09:25 UTC
-    #     if format == 'year':
-    #         return strDate.strftime(forYear)
-    #     elif format == 'short':
-    #         return strDate.strftime(forShort)
-    #     elif format == 'long':
-    #         return strDate.strftime(forLong) + 'UTC'
-
-    # def printShort(self):
-    #     print(self.episodeNum)
-    #     print("Title = " + self.title)
-    #     print("Subtitle = " + self.subtitle)
-    #     print("Author = " + self.author)
-    #     print("Published on (short) = "+ self.formatPubDate('short'))
-    #     print("Published on (long) = " + self.formatPubDate('long'))
-    #     print("Published year = " + self.pubYear)
-
-    # def printStory(self):
-    #     print("Story = " + self.story)
-
     def getFileName(self):
         formatEpiNum = str(self.getEpisodeNum()).translate(str.maketrans('','',string.punctuation))
         formatEpiNum = formatEpiNum.replace(" ", "_")
