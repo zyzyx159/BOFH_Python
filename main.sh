22be1a62a51ddeb53ec4c0c5ced2325e871239a0
Daniel Stellmon <Dstellm@gmail.com>
2025-08-03 02:59:47 -0500

    run every 5 minutes for testing

---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Path to your virtual environment activate script
+VENV_PATH=".venv/bin/activate"
+
+# Activate the virtual environment
+source "$VENV_PATH"
+
+# Run your Python program
+python main.py
