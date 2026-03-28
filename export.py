d52801a2479d6cea3c304bc1f57e417236cfcc6f
Daniel Stellmon <Dstellm@gmail.com>
2026-03-28 04:15:39 -0500

    changed dest folder for output

--- export.py
+++ export.py
@@ -16,7 +16,7 @@ class export:
         self.book.set_language('en')
         self.book.add_author('Simon Travaglia')
         self.book.set_cover("image/bofh_phone_cover.png", open('image/bofh_phone_cover.png', 'rb').read())
-        
+
         cover = epub.EpubHtml(title='Cover', file_name='intro.xhtml',lang='en')
         cover.content='''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
             <html xmlns="http://www.w3.org/1999/xhtml"><head>
@@ -26,13 +26,13 @@ class export:
             </body></html>'''
         self.book.add_item(cover)
         self.book.spine.insert(0,cover)
-    
+
     def incrementSpineCount(self):
         self.spineCount = self.spineCount + 1
 
     def getSpineCount(self):
         return self.spineCount
-    
+
     def buildBook(self):
         # get sorted list of years
         years = self.DB.getPubYears()
@@ -40,7 +40,7 @@ class export:
         for year in years:
             toc.startSection(year[0])
             links = self.DB.getLinksByYear(year[0]) #links by year ordered by episodeNum ASC
-            for link in links: 
+            for link in links:
                 epi = episode.episode(link[0]) # build episode per link
                 epi.DBInit()
                 toc.addChapter(epi.getFileName(), epi.getChapterTitle())
@@ -56,4 +56,4 @@ class export:
         self.book.add_item(epub.EpubNcx())
         self.book.add_item(epub.EpubNav())
 
-        epub.write_epub('/app/output/bofh.epub', self.book)
+        epub.write_epub('/home/zyzyx/git/BOFH_Python/app/output/bofh.epub', self.book)
